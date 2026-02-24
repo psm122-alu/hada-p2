2d84e25a7fddb4d34a631a84e63a540275bddb63
psm122-alu <psm122@alu.ua.es>
2026-02-24 17:17:47 +0100

    20520990E Clase Eventos.cs hecha

--- hada-p2/Eventos.cs
+++ hada-p2/Eventos.cs
@@ -6,7 +6,27 @@ using System.Threading.Tasks;
 
 namespace Hada
 {
-    internal class Eventos
+    public class TocadoArgs : EventArgs
     {
+        public string Nombre { get; private set; }
+        public Coordenada CoordenadaImpacto { get; private set; }
+
+        public TocadoArgs(string nombre, Coordenada coordenadaImpacto)
+        {
+            this.Nombre = nombre;
+            this.CoordenadaImpacto = coordenadaImpacto;
+        }
+
+    }
+
+    public class HundidoArgs : EventArgs { 
+        public string Nombre { get; private set; }
+
+        public HundidoArgs(string nombre)
+        {
+            this.Nombre = nombre;
+        }
+
+
     }
 }
