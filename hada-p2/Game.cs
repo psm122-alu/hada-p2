20850d9622501b3f89b4a3b87d82b4b040fcbea1
prg88-ua <prg88@alu.ua.es>
2026-03-01 19:45:02 +0100

    46081260R Games.cs acabado

--- hada-p2/Game.cs
+++ hada-p2/Game.cs
@@ -1,12 +1,66 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Hada
 {
-    internal class Game
+    public class Game
     {
+        private bool finPartida;
+
+        public Game() 
+        {
+            finPartida = false;
+            gameLoop();
+        }
+
+        private void gameLoop()
+        {
+            List<Barco> barcos = new List<Barco>
+            {
+                new Barco("THOR", 1, 'h', new Coordenada(0,0)),
+                new Barco("LOKI", 2, 'v', new Coordenada(1,2)),
+                new Barco("MAYA", 3, 'h', new Coordenada(3,1))
+            };
+
+            Tablero tablero = new Tablero(4, barcos);
+
+            tablero.eventoFinPartida += cuandoEventoFinPartida;
+
+            while (!finPartida)
+            {
+                Console.WriteLine(tablero.ToString());
+                Console.WriteLine("Introduce la coordenada a disparar ('s' o 'S' para salir):");
+
+                string entrada = Console.ReadLine();
+
+                if (entrada.ToLower() == "s")
+                {
+                    finPartida = true;
+                    break;
+                }
+
+                string[] partes = entrada.Split(',');
+
+                if (partes.Length == 2)
+                {
+                    Coordenada coordenadaDisparo = new Coordenada(partes[0], partes[1]);
+                    tablero.Disparar(coordenadaDisparo);
+
+                }
+                else
+                {
+                    Console.WriteLine("Formato introducido incorrecto, porfavor introduce otra vez la coordenada");
+                }
+            }
+        }
+        private void cuandoEventoFinPartida(object sender, EventArgs e)
+        {
+            Console.WriteLine("PARTIDA FINALIZADA!!");
+            finPartida = true;
+        }
     }
 }
